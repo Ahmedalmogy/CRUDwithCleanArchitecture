9dfe125ec1e296a8b76831c096e52b5b87c150e1
TIBA <TIBA@DESKTOP-065L8GK>
2024-05-14 15:21:24 +0300

    add a class to add services to dependency injection container

---
+++ Infrastructur/DependencyInjection/ServiceContainer.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Entity.Authentication;
+using Infrastructur.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructur.DependencyInjection
+{
+    public static class ServiceContainer
+    {
+        public static IServiceCollection AddInfrastructureService(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(config.GetConnectionString("Default")));
+            services.AddIdentityCore<ApplicationUser>()
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<AppDbContext>()
+                .AddSignInManager ();
+            return services; 
+        }
+    }
+}
