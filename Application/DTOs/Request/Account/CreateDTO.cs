3761a4e5ed8ee1ec4c8a9704fc0e3eec663692e4
TIBA <TIBA@DESKTOP-065L8GK>
2024-05-14 11:59:55 +0300

    add createdto

---
+++ Application/DTOs/Request/Account/CreateDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs.Request.Account
+{
+    public class CreateDTO:LoginDTO
+    {
+        [Required]
+        public string  Name { get; set; }
+        [Required,Compare(nameof(Password))]
+        public string ConfirmedPassword { get; set; }=string.Empty;
+        public string Role { get; set; } = string.Empty;
+
+    }
+}
