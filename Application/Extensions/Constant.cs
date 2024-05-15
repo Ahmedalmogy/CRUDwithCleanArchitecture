d55c27baf2b81548bb8c5c559ebba5ffafb5e9ba
TIBA <TIBA@DESKTOP-065L8GK>
2024-05-15 11:51:15 +0300

    Add constant class to application

---
+++ Application/Extensions/Constant.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Extensions
+{
+    public static class Constant
+    {
+        public static class Role
+        { 
+            public const string Admin = "Admin";
+            public const string user = "User";
+        
+        }
+    }
+}
