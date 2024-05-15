150cc86dd623675345c2edf499e0d869276e99eb
TIBA <TIBA@DESKTOP-065L8GK>
2024-05-15 11:49:59 +0300

    create IAccount Interface

---
+++ Application/Contracts/IAccount.cs
@@ -0,0 +1,24 @@
+﻿using Application.DTOs.Request.Account;
+using Application.DTOs.Request;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.DTOs.Response;
+using Application.DTOs.Response.Account;
+
+namespace Application.Contracts
+{
+    public interface IAccount
+    {
+        Task CreateAdmin();
+        Task<GeneralResponseDTO> CreateAccountAsyc(CreateDTO model);
+        Task<LoginResponseDTO> LoginAccountAsync(LoginDTO model);
+
+        Task<GeneralResponseDTO> CreateRoleAsync(CreateRoleDTO model);
+        Task <IEnumerable<GetRoleDTO>> GetRolesAsync();
+        Task <IEnumerable<GetUsersWithRolesResponseDTO>>GetUsersWithRolesAsync();
+        Task ChangeUserRoleAsync(ChangeUserRoleRequestDTO model);
+
+    }
+}
